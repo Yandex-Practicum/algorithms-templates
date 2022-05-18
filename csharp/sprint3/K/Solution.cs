5a4a3cf56364b83982eea33cffbef609f5dad07c
Polina Romanchenko <polinaromanchenko2000@gmail.com>
2022-05-18 12:07:46 +0300

    added sprint 3

---
+++ csharp/sprint3/K/Solution.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public static void MergeSort(List<int> array, int left, int right)
+    {
+        // Your code
+        // “ヽ(´▽｀)ノ”
+    }
+
+    public static List<int> Merge(List<int> array, int left, int mid, int right)
+    {
+        // Your code
+        // “ヽ(´▽｀)ノ”
+    }
+
+    public static void Main(string[] args)
+    {
+        var a = new List<int> { 1, 4, 9, 2, 10, 11 };
+        var b = Merge(a, 0, 3, 6);
+        var expectedMergeResult = new List<int> {1, 2, 4, 9, 10, 11};
+        System.Console.WriteLine(b.SequenceEqual(expectedMergeResult));
+        var c = new List<int> {1, 4, 2, 10, 1, 2};
+        MergeSort(c, 0, 6);
+        var expectedMergeSortResult = new List<int> {1, 1, 2, 2, 4, 10};
+        System.Console.WriteLine(c.SequenceEqual(expectedMergeSortResult));
+    }
+}
---
+++ python/sprint3/K/code.py
@@ -0,0 +1,20 @@
+def merge(arr, lf, mid, rg):
+	# Your code
+	# “ヽ(´▽｀)ノ”
+	pass
+
+
+def merge_sort(arr, lf, rg):
+	# Your code
+	# “ヽ(´▽｀)ノ”
+	pass
+
+def test():
+	a = [1, 4, 9, 2, 10, 11]
+	b = merge(a, 0, 3, 6)
+	expected = [1, 2, 4, 9, 10, 11]
+	assert b == expected
+	c = [1, 4, 2, 10, 1, 2]
+	merge_sort(c, 0 , 6)
+	expected = [1, 1, 2, 2, 4, 10]
+	assert c == expected
