84a39f39bc435f3f9c90615e9a11a57ecbccc8dd
Polina Romanchenko <polinaromanchenko2000@gmail.com>
2022-11-07 11:26:47 +0300

    fixed Cs template

--- csharp/sprint5/A/Solution.cs
+++ csharp/sprint5/A/Solution.cs
@@ -4,14 +4,7 @@ public class Solution
 {
     public static int Solve(Node root)
     {
-        if (root == null)
-        {
-            return -1;
-        }
-
-        var ans = root.Value;
-        ans = Math.Max(Solve(root.Left), ans);
-
-        return Math.Max(Solve(root.Right), ans);
+        // Your code
+        // “ヽ(´▽｀)ノ”
     }
 }
