b337d3be5c722061e62d8f96e32c092027e3c2c7
Andrey Alyonin <huntex@yandex.ru>
2023-02-11 18:51:55 +0400

    fixed template for problem D sprint2

--- csharp/sprint2/D/Solution.cs
+++ csharp/sprint2/D/Solution.cs
@@ -28,7 +28,7 @@ public class Solution
         var node2 = new Node<string>("node2", node3);
         var node1 = new Node<string>("node1", node2);
         var node0 = new Node<string>("node0", node1);
-        var idx = Solution<string>.Solve(node0, "node2");
+        var idx = Solution.Solve(node0, "node2");
         // result is : idx == 2
     }
 }
