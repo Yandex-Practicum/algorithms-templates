9dbb0c8a36ef63c64ce8722d7470ddacc69020c0
Romanchenko <po2013ly@yandex.ru>
2023-02-24 04:24:36 +0300

    kotlin templates for free course

---
+++ kotlin/sprint0/A.kt
@@ -0,0 +1,16 @@
+private fun getSum(a: Int, b: Int): Int {
+    TODO()
+    // Your code
+    // ヽ(´▽`)/
+}
+
+fun main(args: Array<String>) {
+    val a = readInt()
+    val b = readInt()
+    println(getSum(a, b))
+}
+
+private fun readStr() = readln()
+private fun readInt() = readStr().toInt()
+private fun readStrings() = readStr().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
---
+++ kotlin/sprint0/C.kt
@@ -0,0 +1,17 @@
+private fun movingAverage(a: List<Int>, k: Int): List<Double> {
+    TODO()
+    // Your code
+    // ヽ(´▽`)/
+}
+
+fun main() {
+    val size = readInt()
+    val a = readInts()
+    val k = readInt()
+    println(movingAverage(a, k).joinToString(" "))
+}
+
+private fun readStr() = readln()
+private fun readInt() = readStr().toInt()
+private fun readStrings() = readStr().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
