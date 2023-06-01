127fc5a0c615535d4aca7c639d656ff5dc7b4b95
pmromanchenko <pmromanchenko@avito.ru>
2023-06-02 01:09:10 +0300

    added 2 and 3 sprints

---
+++ kotlin/sprint2/D/Solution.kt
@@ -0,0 +1,19 @@
+/**
+ * Comment it before submitting
+ *
+ * class Node<V>(var value: V, var next: Node<V>?) {}
+ */
+
+fun solution(head: Node<String>?, item: String): Int {
+    // Your code
+    // ヽ(´▽`)/
+}
+
+fun test() {
+    val node3 = Node("node3", null)
+    val node2 = Node("node2", node3)
+    val node1 = Node("node1", node2)
+    val node0 = Node("node0", node1)
+    val idx: Int = solution(node0, "node2")
+    assert(idx == 2)
+}
