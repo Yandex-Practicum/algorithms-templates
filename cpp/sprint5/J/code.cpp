6d1f7a6c12644eca42637d8446a40939aa323d01
Romanchenko <po2013ly@yandex.ru>
2023-06-04 22:06:51 +0300

    fixed test

--- cpp/sprint5/J/code.cpp
+++ cpp/sprint5/J/code.cpp
@@ -24,9 +24,9 @@ Node* insert(Node* root, int key) {
 
 #ifndef REMOTE_JUDGE
 void test() {
-    Node node1({nullptr, nullptr, 7});
-    Node node2({&node1, nullptr, 8});
-    Node node3({nullptr, &node2, 7});
+    Node node1(7, nullptr, nullptr);
+    Node node2(8, &node1, nullptr);
+    Node node3(7, nullptr, &node2);
     Node* newHead = insert(&node3, 6);
     assert(newHead->left->value == 6);
     assert(newHead == &node3);
