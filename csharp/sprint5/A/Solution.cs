9196b206a97a65c8bbd33e9ede0be47dc68bf68c
pmromanchenko <pmromanchenko@avito.ru>
2023-06-25 21:51:33 +0300

    added test to the csharp 5A

--- csharp/sprint5/A/Solution.cs
+++ csharp/sprint5/A/Solution.cs
@@ -1,10 +1,44 @@
 using System;
 
+/*
+// закомментируйте перед отправкой
+public class Node
+{
+    public int Value { get; set; }
+    public Node Left { get; set; }
+    public Node Right { get; set; }
+
+    public Node(int value)
+    {
+        Value = value;
+        Left = null;
+        Right = null;
+    }
+}
+*/
+
 public class Solution
 {
     public static int Solve(Node root)
     {
         // Your code
         // “ヽ(´▽｀)ノ”
+    }
+
+
+    private static void Test()
+    {
+        var node1 = new Node(1);
+        var node2 = new Node(-5);
+        var node3 = new Node(3)
+        {
+            Left = node1,
+            Right = node2
+        };
+        var node4 = new Node(2)
+        {
+            Left = node3
+        };
+        Console.WriteLine(Solve(node5));
     }
 }
